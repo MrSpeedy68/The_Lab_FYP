82c949ff6fa67b448565ed6484154d101efa76ff
Adrian Hebel <47033681+MrSpeedy68@users.noreply.github.com>
2021-11-01 21:08:04 +0000

    Locomotion system in VR

---
+++ Assets/ContinuousMovement.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class ContinuousMovement : MonoBehaviour
+{
+    public float speed = 1f;
+    public XRNode inputSource;
+    public float gravity = -9.81f;
+    public LayerMask groundLayer;
+    public float additionalHeight = 0.2f;
+
+    private float fallingSpeed;
+    private XRRig rig;
+    private Vector2 inputAxis;
+    private CharacterController character;
+
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+        rig = GetComponent<XRRig>();
+    }
+
+    void Update()
+    {
+        InputDevice device = InputDevices.GetDeviceAtXRNode(inputSource);
+        device.TryGetFeatureValue(CommonUsages.primary2DAxis, out inputAxis); //Get controller and the output joystick values
+    }
+
+    private void FixedUpdate()
+    {
+        CapsuleFollowHeadset();
+        Quaternion headYaw = Quaternion.Euler(0, rig.cameraGameObject.transform.eulerAngles.y, 0); //Get the rotation of the head
+        Vector3 direction = headYaw * new Vector3(inputAxis.x, 0, inputAxis.y); //always move towards the direction of the head
+
+        character.Move(direction * Time.fixedDeltaTime * speed);
+
+        //Gravity
+        bool isGrounded = CheckIfGrounded();
+        if(isGrounded)
+        {
+            fallingSpeed = 0;
+        }
+        else
+        {
+            fallingSpeed += gravity * Time.fixedDeltaTime;
+        }
+
+        character.Move(Vector3.up * fallingSpeed * Time.fixedDeltaTime);
+    }
+
+    void CapsuleFollowHeadset()
+    {
+        character.height = rig.cameraInRigSpaceHeight + additionalHeight;
+        Vector3 capsuleCenter = transform.InverseTransformPoint(rig.cameraGameObject.transform.position);
+        character.center = new Vector3(capsuleCenter.x, character.height / 2 + character.skinWidth, capsuleCenter.z);
+    }
+
+    //A check to see if the player is on the ground
+    bool CheckIfGrounded()
+    {
+        Vector3 rayStart = transform.TransformPoint(character.center);
+        float rayLength = character.center.y + 0.01f;
+        bool hashit = Physics.SphereCast(rayStart, character.radius, Vector3.down, out RaycastHit hitInfo, rayLength, groundLayer);
+
+        return hashit;
+    }
+}
