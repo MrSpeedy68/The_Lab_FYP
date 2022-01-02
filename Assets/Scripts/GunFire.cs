b8928d6996c88ff85e3922efa0249bc037683715
Adrian Hebel <47033681+MrSpeedy68@users.noreply.github.com>
2022-01-02 22:25:44 +0000

    Fixed bullet collision detection

--- Assets/Scripts/GunFire.cs
+++ Assets/Scripts/GunFire.cs
@@ -1,10 +1,11 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GunFire : MonoBehaviour
 {
-    public float speed = 20f;
+    public float speed = 60f;
     public GameObject bullet;
     public Transform barrel;
     public AudioSource audioSource;
@@ -15,6 +16,18 @@ public class GunFire : MonoBehaviour
         GameObject spawnedBullet = Instantiate(bullet, barrel.position, barrel.rotation);
         spawnedBullet.GetComponent<Rigidbody>().velocity = speed * barrel.forward;
         audioSource.PlayOneShot(audioClip);
-        Destroy(spawnedBullet, 2);
+        Destroy(spawnedBullet, 5);
     }
+
+    // private void OnCollisionEnter(Collision other)
+    // {
+    //     if (other.rigidbody != null)
+    //     {
+    //         Vector3 hitVector = (other.transform.position - transform.position).normalized;
+    //     
+    //         other.rigidbody.AddForce(-hitVector * other.rigidbody.mass * 100f);
+    //         
+    //         Debug.Log("Hit Rigid body and added force");
+    //     }
+    // }
 }
