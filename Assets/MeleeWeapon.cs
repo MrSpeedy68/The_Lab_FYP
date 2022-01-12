5a5666fb808f00e568b59831aec50bf9f7b1e877
Adrian Hebel <47033681+MrSpeedy68@users.noreply.github.com>
2022-01-12 18:57:53 +0000

    Fixed Melee Weapons Movement

--- Assets/MeleeWeapon.cs
+++ Assets/MeleeWeapon.cs
@@ -1,13 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations.Rigging;
 
 public class MeleeWeapon : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rigidbody rb = GetComponent<Rigidbody>();
+
+        if(rb != null) rb.maxAngularVelocity = 20;
     }
 
     // Update is called once per frame
