b31c297eba1c75144ffaf28a846ffbde9cacbe6e
Adrian Hebel <47033681+MrSpeedy68@users.noreply.github.com>
2022-01-19 21:38:57 +0000

    Automatic Rifle Firing

--- Assets/Scripts/GunComponent.cs
+++ Assets/Scripts/GunComponent.cs
@@ -6,7 +6,8 @@ using UnityEngine;
 public class GunComponent : MonoBehaviour
 {
 
-    public float speed = 60f;
+    public float bulletSpeed = 60f;
+    public float fireRate = 12f; // How many rounds fired per second average fire rate = 700/min
     public GameObject bullet;
     public Transform barrel;
     public AudioSource audioSource;
@@ -19,18 +20,19 @@ public class GunComponent : MonoBehaviour
     public GameObject magazineSocket;
 
     private MagazineComponent _magazineComponent;
+    private bool _isActive = false;
     public void Fire()
     {
         _magazineComponent = magazineSocket.GetComponentInChildren<MagazineComponent>();
 
         if (_magazineComponent != null)
         {
-            Debug.Log(_magazineComponent.ammoCount);
+            Debug.Log(_magazineComponent.currentAmmoCount);
 
-            if (_magazineComponent.ammoCount > 0)
+            if (_magazineComponent.currentAmmoCount > 0)
             {
                 GameObject spawnedBullet = Instantiate(bullet, barrel.position, barrel.rotation);
-                spawnedBullet.GetComponent<Rigidbody>().velocity = speed * barrel.forward;
+                spawnedBullet.GetComponent<Rigidbody>().velocity = bulletSpeed * barrel.forward;
                 audioSource.volume = 50f;
                 audioSource.PlayOneShot(gunFireAudio);
                 Destroy(spawnedBullet, 5);
@@ -39,6 +41,34 @@ public class GunComponent : MonoBehaviour
             else audioSource.PlayOneShot(gunEmptyAudio);
         }
         else audioSource.PlayOneShot(gunEmptyAudio);
+    }
+
+    private float timeBeforeShooting;
+
+    private void Start()
+    {
+        timeBeforeShooting = 1 / fireRate;
+    }
+
+    private void Update()
+    {
+        if (_isActive)
+        {
+            if (timeBeforeShooting <= 0f)
+            {
+                Fire();
+                timeBeforeShooting = 1 / fireRate;
+            }
+            else
+            {
+                timeBeforeShooting -= Time.deltaTime;
+            }
+        }
+    }
+
+    public void WeaponState()
+    {
+        _isActive = !_isActive;
     }
 
     public void PlayMagIn()
--- Assets/Scripts/MagazineComponent.cs
+++ Assets/Scripts/MagazineComponent.cs
@@ -6,22 +6,22 @@ using UnityEngine;
 
 public class MagazineComponent : MonoBehaviour
 {
-    public int ammoCount = 0;
+    public int currentAmmoCount = 0;
     
     [SerializeField] private TMP_Text _ammoText;
     
-    private int _maxAmmo = 20;
+    [SerializeField]private int _maxAmmo = 30;
 
     private void Start()
     {
-        ammoCount = _maxAmmo;
+        currentAmmoCount = _maxAmmo;
 
-        _ammoText.text = ammoCount + "/" + _maxAmmo;
+        _ammoText.text = currentAmmoCount + "/" + _maxAmmo;
     }
 
     public void RemoveBullet()
     {
-        ammoCount--;
-        _ammoText.text = ammoCount + "/" + _maxAmmo;
+        currentAmmoCount--;
+        _ammoText.text = currentAmmoCount + "/" + _maxAmmo;
     }
 }
