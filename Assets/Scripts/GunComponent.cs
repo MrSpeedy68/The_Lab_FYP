ad11db9be0aee40fd40dd966a285fefc4ca65bcd
Adrian Hebel <47033681+MrSpeedy68@users.noreply.github.com>
2022-01-20 22:29:56 +0000

    Revamped Gun to Raycast shooting

--- Assets/Scripts/GunComponent.cs
+++ Assets/Scripts/GunComponent.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GunComponent : MonoBehaviour
 {
@@ -9,7 +10,7 @@ public class GunComponent : MonoBehaviour
     [SerializeField] private float bulletSpeed = 60f;
     [SerializeField] private float fireRate = 12f; // How many rounds fired per second average fire rate = 700/min
     [SerializeField] private GameObject bullet;
-    [SerializeField] private Transform barrel;
+    [SerializeField] private Transform bulletSpawnPoint;
     [SerializeField] private AudioSource audioSource;
     
     [SerializeField] private AudioClip gunFireAudio;
@@ -21,26 +22,33 @@ public class GunComponent : MonoBehaviour
     [SerializeField] private GameObject casing;
     [SerializeField] private Transform ejectTransform;
 
+    [SerializeField] private TrailRenderer bulletTrail;
+    [SerializeField] private ParticleSystem shootParticleSystem;
+    [SerializeField] private ParticleSystem impactParticleSystem;
+
+    public LayerMask _mask;
     private MagazineComponent _magazineComponent;
     private bool _isActive = false;
-    private float timeBeforeShooting;
+    private float _timeBeforeShooting;
     
     public void Fire()
     {
         _magazineComponent = magazineSocket.GetComponentInChildren<MagazineComponent>();
-
-        if (_magazineComponent != null)
+        if (_magazineComponent != null && _magazineComponent.currentAmmoCount > 0)
         {
-            Debug.Log(_magazineComponent.currentAmmoCount);
-
-            if (_magazineComponent.currentAmmoCount > 0)
+            if (Physics.Raycast(bulletSpawnPoint.position, bulletSpawnPoint.forward, out RaycastHit hit, float.MaxValue, _mask))
             {
-                GameObject spawnedBullet = Instantiate(bullet, barrel.position, barrel.rotation);
-                spawnedBullet.GetComponent<Rigidbody>().velocity = bulletSpeed * barrel.forward;
+                // GameObject spawnedBullet = Instantiate(bullet, bulletSpawnPoint.position, bulletSpawnPoint.rotation);
+                // spawnedBullet.GetComponent<Rigidbody>().velocity = bulletSpeed * bulletSpawnPoint.forward;
+                // Destroy(spawnedBullet, 5);
+
+                StartCoroutine(SpawnTrail(bulletTrail, hit));
+                
                 audioSource.volume = 50f;
                 audioSource.PlayOneShot(gunFireAudio);
-                Destroy(spawnedBullet, 5);
                 _magazineComponent.RemoveBullet();
+
+                shootParticleSystem.Play();
                 EjectCasing();
             }
             else audioSource.PlayOneShot(gunEmptyAudio);
@@ -48,32 +56,48 @@ public class GunComponent : MonoBehaviour
         else audioSource.PlayOneShot(gunEmptyAudio);
     }
 
+    IEnumerator SpawnTrail(TrailRenderer Trail, RaycastHit Hit)
+    {
+        float time = 0f;
+        Vector3 startPosition = Trail.transform.position;
 
+        while (time < 1)
+        {
+            Trail.transform.position = Vector3.Lerp(startPosition, Hit.point, time);
+            time += Time.deltaTime / Trail.time;
 
+            yield return null;
+        }
+        Trail.transform.position = Hit.point;
+        Instantiate(impactParticleSystem, Hit.point, Quaternion.LookRotation(Hit.normal));
+        
+        Destroy(Trail.gameObject, Trail.time);
+    }
+    
     private void Start()
     {
-        timeBeforeShooting = 1 / fireRate;
+        _timeBeforeShooting = 1 / fireRate;
     }
 
     private void Update()
     {
         if (_isActive)
         {
-            if (timeBeforeShooting <= 0f)
+            if (_timeBeforeShooting <= 0f)
             {
                 Fire();
-                timeBeforeShooting = 1 / fireRate;
+                _timeBeforeShooting = 1 / fireRate;
             }
             else
             {
-                timeBeforeShooting -= Time.deltaTime;
+                _timeBeforeShooting -= Time.deltaTime;
             }
         }
     }
 
     private void EjectCasing()
     {
-        var ejectedCasing = Instantiate(casing, ejectTransform.position, Quaternion.Euler(90f,0f,0f));
+        var ejectedCasing = Instantiate(casing, ejectTransform.position, Quaternion.Euler(90f,UnityEngine.Random.Range(-45f, 45f),UnityEngine.Random.Range(-45f, 45f)));
         var rb = ejectedCasing.GetComponent<Rigidbody>();
         rb.AddForce(ejectTransform.right.normalized * 300);
         Destroy(ejectedCasing, 3f);
